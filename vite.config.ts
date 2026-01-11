ecac371379942148448e59394945cffbb5c9118e
Haider Al-Delfi <haider87@outlook.com>
2026-01-11 16:01:53 +0000

    Attempt to fix deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    base: "/gayholiday",
+    base: "/",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
