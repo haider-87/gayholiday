056c8189e5ff0fcb819913c3f1503c018c217bc8
Haider Al-Delfi <haider87@outlook.com>
2026-01-11 16:12:50 +0000

    Updated base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    base: "/",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
@@ -16,4 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
 }));
